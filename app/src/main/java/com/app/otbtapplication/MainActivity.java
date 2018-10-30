29d900619b2c318a99d30fee36e8c7d9789a6d50
kylesanne <k.sanne@hotmail.com>
2018-10-30 20:19:41 +1300

    *

--- app/src/main/java/com/app/otbtapplication/MainActivity.java
+++ app/src/main/java/com/app/otbtapplication/MainActivity.java
@@ -29,35 +29,24 @@ public class MainActivity extends AppCompatActivity {
     private void getImages(){
         Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
 
-        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
-        mNames.add("Havasu Falls");
+        mImageUrls.add("https://i.imgur.com/bx9MyZr.jpg");
+        mNames.add("Glamping");
 
-        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
-        mNames.add("Trondheim");
+        mImageUrls.add("https://i.imgur.com/uADPfu5.jpg");
+        mNames.add("Experiences");
 
-        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
-        mNames.add("Portugal");
+        mImageUrls.add("https://i.imgur.com/Gn0Zydn.jpg");
+        mNames.add("Camping");
 
-        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
-        mNames.add("Rocky Mountain National Park");
+        mImageUrls.add("https://i.imgur.com/Gn0Zydn.jpg");
+        mNames.add("Example");
 
+        mImageUrls.add("https://i.imgur.com/Gn0Zydn.jpg");
+        mNames.add("Example");
 
-        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
-        mNames.add("Mahahual");
-
-        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
-        mNames.add("Frozen Lake");
-
-
-        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
-        mNames.add("White Sands Desert");
-
-        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
-        mNames.add("Austrailia");
-
-        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
-        mNames.add("Washington");
-
+        mImageUrls.add("https://i.imgur.com/Gn0Zydn.jpg");
+        mNames.add("Example");
+        
         initRecyclerView();
     }
 
