ae430e69db1a47495423637b21461cad68b60bb1
William <wwdouglas@hotmail.co.uk>
2018-11-04 12:59:11 +1300

    Camping, all activities and image resizing

--- app/src/main/java/com/app/otbtapplication/Camping.java
+++ app/src/main/java/com/app/otbtapplication/Camping.java
@@ -1,13 +1,52 @@
 package com.app.otbtapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Camping extends AppCompatActivity {
+
+    private static ImageView cone;
+    private static ImageView ctwo;
+    private static ImageView cthree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camping);
+        OnclickButtonListener();
+    }
+
+    public void OnclickButtonListener() {
+
+        cone =  findViewById(R.id.imageView_campingOne);
+        ctwo =  findViewById(R.id.imageView_campingTwo);
+        cthree =  findViewById(R.id.imageView_campingThree);
+
+        cone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Camping.this,CampingOne.class);
+                startActivity(intent);
+            }
+        });
+
+        ctwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Camping.this,CampingTwo.class);
+                startActivity(intent);
+            }
+        });
+
+        cthree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Camping.this,CampingThree.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/app/otbtapplication/CampingThree.java
@@ -0,0 +1,13 @@
+package com.app.otbtapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CampingThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camping_three);
+    }
+}
