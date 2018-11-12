44aaca696ab22ea751dea1104c71c5029ca69007
kylesanne <k.sanne@hotmail.com>
2018-11-12 17:38:13 +1300

    Updated by Kyle

--- app/src/main/java/com/app/otbtapplication/BookActivity.java
+++ app/src/main/java/com/app/otbtapplication/BookActivity.java
@@ -2,12 +2,19 @@ package com.app.otbtapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BookActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book);
-    }
 }
+
+
+
+
+
