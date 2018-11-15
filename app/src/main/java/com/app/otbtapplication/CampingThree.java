bad5ec0b69afd742ea03c2b74382a02ba36b22d7
kylesanne <k.sanne@hotmail.com>
2018-11-16 02:11:20 +1300

    Updated by Kyle

--- app/src/main/java/com/app/otbtapplication/CampingThree.java
+++ app/src/main/java/com/app/otbtapplication/CampingThree.java
@@ -1,13 +1,45 @@
 package com.app.otbtapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class CampingThree extends AppCompatActivity {
+public class CampingThree extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Button booking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camping_three);
+
+        Button booking = (Button) findViewById(R.id.bookButton);
+
+
+        booking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenActivityBook();
+            }
+        });
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+        }}
+
+
+    public void OpenActivityBook() {
+        Intent intent = new Intent(this, BookActivity.class);
+        startActivity(intent);
+    }
+
+    
 }
